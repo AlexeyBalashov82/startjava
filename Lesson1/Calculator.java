58e8d5efd892bcdd0247197570e5cb991ef22e95
Алексей Балашов <aleksejbalasov@MacBook-Pro-Aleksej.local>
2022-01-13 13:54:34 +0300

    Calculator task added

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    private static int firstNumber = 26;
+    private static int secondNumber = 10;
+    private static char action = '/';
+
+    public static void main(String[] args) {
+        if (action == '+') {
+            System.out.println(firstNumber+secondNumber);
+        } else if (action == '-') {
+            System.out.println(firstNumber-secondNumber);
+        } else if (action == '*') {
+            System.out.println(firstNumber*secondNumber);            
+        } else if (action =='/') {
+            System.out.println(firstNumber/(double)secondNumber);            
+        } else if (action == '^') {
+           long power = 1;
+           for (int i = 1; i<=secondNumber; i++){
+             power *= firstNumber;
+           } 
+            System.out.println(power);
+        } else if (action =='%') {
+            System.out.println(firstNumber%secondNumber);            
+        }
+    }
+}
