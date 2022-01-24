05f3ea337af5511deb7271fda9aa4dfdf2c154a5
AlexB82 <82.abalashov@gmail.com>
2022-01-24 16:14:26 +0300

    Wolf and WolfTest clasess added

--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -4,7 +4,7 @@ public class WolfTest {
         Wolf wolf = new Wolf(false, "Honey", 10, 3, "Grey");
         System.out.println("Is this wolf a male? " + wolf.isMale);
         System.out.println("The name of this wolf is " + wolf.name);        
-        System.out.println("This wolf wieghts: " + wolf.weight+" kg.");
+        System.out.println("This wolf wieghts: " + wolf.weight + " kg.");
         System.out.println("This wolf is " + wolf.age + " years old");
         System.out.println("The color of the wold is " + wolf.color);
         wolf.sit();
