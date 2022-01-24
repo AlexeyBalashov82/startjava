6fe777c4e8179df02fdd2b173734572f555deb15
AlexB82 <82.abalashov@gmail.com>
2022-01-24 14:34:06 +0300

    Person class added

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    boolean isMale = true;
+    String name = "Alex";
+    double height = 1.76;
+    int weight = 96;
+    int age = 39;
+
+    void sit() {
+        System.out.println("Sitting");
+    }
+
+    void walk() {
+        System.out.println("Walking");
+    }
+
+    void run() {
+        System.out.println("Running");
+    }
+
+    String talk() {
+        return "Hi, bro!";
+    }
+
+    void learnJava() {
+        System.out.println("Java f*cking great!");
+    }
+}
