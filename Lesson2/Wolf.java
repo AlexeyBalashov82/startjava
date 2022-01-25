09b934b16893608cb09d3b26af0fcfc23ff75212
AlexB82 <82.abalashov@gmail.com>
2022-01-25 14:48:49 +0300

    Getter and setters added to Wolf class

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,10 +1,54 @@
 public class Wolf {
 
-    boolean isMale;
-    String name;
-    int weight;
-    int age;
-    String color;
+    private boolean sex; // true - male, false - female
+    private String name;
+    private int weight;
+    private int age;
+    private String color;
+
+    public boolean isMale() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age>8){
+            System.out.println("This wolf is seemed to be dead");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     void sit() {
         System.out.println("Sitting");
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -2,20 +2,15 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.isMale = false;
-        wolf.name = "Honey";
-        wolf.weight = 10;
-        wolf.age = 3;
-        wolf.color = "Grey";
-        System.out.println("Is this wolf a male? " + wolf.isMale);
-        System.out.println("The name of this wolf is " + wolf.name);        
-        System.out.println("This wolf wieghts: " + wolf.weight + " kg.");
-        System.out.println("This wolf is " + wolf.age + " years old");
-        System.out.println("The color of the wold is " + wolf.color);
-        wolf.sit();
-        wolf.walk();
-        wolf.run();
-        wolf.howl();
-        wolf.hunt();
+        wolf.setSex(false);
+        wolf.setName("Honey");
+        wolf.setWeight(10);
+        wolf.setAge(3);
+        wolf.setColor("Grey");
+        System.out.println("Is this wolf a male? " + wolf.isMale());
+        System.out.println("The name of this wolf is " + wolf.getName());        
+        System.out.println("This wolf wieghts: " + wolf.getWeight() + " kg.");
+        System.out.println("This wolf is " + wolf.getAge() + " years old");
+        System.out.println("The color of the wold is " + wolf.getColor());
     }
 }
