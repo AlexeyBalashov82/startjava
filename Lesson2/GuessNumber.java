3a58685f309e732c83dbdad898d0d7d859c7fc2a
AlexB82 <82.abalashov@gmail.com>
2022-01-31 18:23:26 +0300

    GuessNumber game refactored

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -1,16 +1,50 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumber {
 
     private int secretNumber;
 
-    public void generateNumber() {
+    public void play(Player player1, Player player2, Scanner console){
+        boolean isfirstPlayerTurn;
+        boolean isSecretNumberGuessed;
+        byte sign;
+
+        generateNumber();
+        isfirstPlayerTurn = true;
+        isSecretNumberGuessed = false;
+        while (!isSecretNumberGuessed){
+            if (isfirstPlayerTurn) {
+                sign = playerNextStep(player1, console);
+            } else {
+                sign = playerNextStep(player2, console);
+            }
+            if (sign<0){
+                System.out.println("Secret number is less then yours.");
+            }  
+            if (sign>0){
+                System.out.println("Secret number is bigger then yours.");
+            }
+            isSecretNumberGuessed = (sign == 0);
+            isfirstPlayerTurn = !isfirstPlayerTurn;
+        }
+        System.out.println("You win!"); 
+    }
+
+    private void generateNumber() {
         Random random = new Random();
         secretNumber = random.nextInt(100) + 1;
     }
 
-    public byte checkNumber(int number) {
+    private byte checkNumber(int number) {
        // -1, если меньше, +1 если больше, 0 - если равно
        return (byte) Integer.signum(secretNumber - number); 
+    }
+
+    private byte playerNextStep (Player player, Scanner console) {
+        System.out.println(player.getName()+", enter your number: ");
+        player.setNumber(console.nextInt());
+        console.nextLine();
+        return checkNumber(player.getNumber());
     }
 }
--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -8,40 +8,11 @@ public class GuessNumberTest {
         Player player1 = new Player(console.nextLine());
         System.out.println("Enter second player name: ");
         Player player2 = new Player(console.nextLine());
-
-        boolean isfirstPlayerTurn;
-        boolean isSecretNumberGuessed;
-        byte sign;
         GuessNumber game = new GuessNumber();
 
         do {
-            game.generateNumber();
-            isfirstPlayerTurn = true;
-            isSecretNumberGuessed = false;
-            while (!isSecretNumberGuessed){
-                if (isfirstPlayerTurn) {
-                    sign = playerNextStep(player1, console, game);
-                } else {
-                    sign = playerNextStep(player2, console, game);
-                }
-              if (sign<0){
-                System.out.println("Secret number is less then yours.");
-              }  
-              if (sign>0){
-                System.out.println("Secret number is bigger then yours.");
-              }
-              isSecretNumberGuessed = (sign == 0);
-              isfirstPlayerTurn = !isfirstPlayerTurn;
-            }
-          System.out.println("You win!");  
+            game.play(player1, player2, console); 
         } while (isNext(console));
-    }
-
-    private static byte playerNextStep (Player player, Scanner console, GuessNumber game) {
-        System.out.println(player.getName()+", enter your number: ");
-        player.setNumber(console.nextInt());
-        console.nextLine();
-        return game.checkNumber(player.getNumber());
     }
 
     private static boolean isNext(Scanner console) {
