b35595e88c300bc447255b69cd38cec6bf2ad1e3
AlexB82 <82.abalashov@gmail.com>
2022-02-07 14:36:50 +0300

    Calculator task. Lesson 4.

---
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package com.startjava.lesson_2_3_4.calculator;
+
+import static java.lang.Math.pow;
+
+class Calculator {
+
+    private String[] mathExpression = new String[3];
+
+    public boolean setMathExpression(String mathExpression){
+        this.mathExpression = mathExpression.split(" ");
+        String validString = "+-*/^%";
+        return validString.contains(this.mathExpression[1]);
+    }
+
+    public double calculate() {
+        switch (mathExpression[1]) {
+            case "+": return Integer.parseInt(mathExpression[0]) + Integer.parseInt(mathExpression[2]);
+            case "-": return Integer.parseInt(mathExpression[0]) - Integer.parseInt(mathExpression[2]);
+            case "*": return Integer.parseInt(mathExpression[0]) * Integer.parseInt(mathExpression[2]);
+            case "/": return Integer.parseInt(mathExpression[0]) / (double) Integer.parseInt(mathExpression[2]);
+            case "^": return pow(Integer.parseInt(mathExpression[0]),Integer.parseInt(mathExpression[2]));
+            case "%": return Integer.parseInt(mathExpression[0]) % Integer.parseInt(mathExpression[2]);
+            default: return 0;
+        }
+    }
+}
---
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.startjava.lesson_2_3_4.calculator;
+
+import java.util.Scanner;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        Scanner console = new Scanner(System.in);
+        String mathExpression;
+        boolean isValidOperation;
+
+        do {
+            do {
+                System.out.print("Enter math expression: ");
+                mathExpression = console.nextLine();
+                isValidOperation = calc.setMathExpression(mathExpression);
+                if (!isValidOperation){
+                    System.out.println("Unknown operation!");
+                }
+            } while (!isValidOperation);
+            System.out.println(mathExpression + " = " + calc.calculate());
+        } while (isNext(console));
+    }
+
+    private static boolean isNext(Scanner console) {
+        String answer;
+        do {
+            System.out.print("Do you want to repeat (yes/no)?: ");
+            answer = console.nextLine();
+        } while (!answer.equals("yes") && !answer.equals("no"));
+        return (answer.equals("yes"));
+    }
+}
